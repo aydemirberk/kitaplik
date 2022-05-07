9ae6d99e27fa3372ff25ebd1ae19b11c0b0c477e
aydemirberk <104275944+aydemirberk@users.noreply.github.com>
2022-05-07 13:54:36 +0300

    Struct Kavramı Dersi
    https://app.patika.dev/courses/csharp-101/5-struct-nedir

---
+++ structkavrami/structkavrami/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace structkavrami
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dikdortgen dikdortgen1 = new Dikdortgen();
+            dikdortgen1.KisaKenar = 3;
+            dikdortgen1.UzunKenar = 4;
+            Console.WriteLine("Class Dikdörtgenin Alanı: {0}", dikdortgen1.AlanHesapla());
+
+            Dikdortgen_Struct dikdortgen_struct = new Dikdortgen_Struct();
+
+            dikdortgen_struct.KisaKenar = 3;
+            dikdortgen_struct.UzunKenar = 4;
+
+            Console.WriteLine("Struct Dikdörtgenin Alanı: {0}", dikdortgen1.AlanHesapla());
+
+        }
+    }
+
+    class Dikdortgen
+    {
+        public int KisaKenar;
+
+        public int UzunKenar;
+
+        public long AlanHesapla()
+        {
+            return this.KisaKenar * this.UzunKenar;
+        }
+    }
+
+    struct Dikdortgen_Struct
+    {
+        public int KisaKenar;
+
+        public int UzunKenar;
+
+        public long AlanHesapla()
+        {
+            return this.KisaKenar * this.UzunKenar;
+        }
+    }
+}
+
